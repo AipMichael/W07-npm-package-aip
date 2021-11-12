101ebfdb5cf5e95fbb16602bc7bd1324b2308cca
AipBorges <mpbds@uic.es>
2021-11-12 12:51:14 +0100

    function deletes files

--- index.js
+++ index.js
@@ -2,16 +2,23 @@ const chalk = require("chalk");
 const fs = require("fs");
 const path = require("path");
 
-const directory = ".";
+const cleanFiles = (directory) => {
+  fs.readdir(directory, (error, files) => {
+    if (error) {
+      console.log(chalk.cyan("Error reading your files."));
+      throw err;
+    } else {
+      for (const file of files) {
+        fs.unlink(path.join(directory, file), (error) => {
+          if (error) {
+            console.log(chalk.yellow("Error deleting your files."));
+            throw error;
+          }
+          console.log(chalk.cyan(`We deleted your file.`));
+        });
+      }
+    }
+  });
+};
 
-fs.readdir(directory, (error, files) => {
-  if (error) throw err;
-
-  for (const file of files) {
-    fs.unlink(path.join(directory, file), (err) => {
-      if (error) throw error;
-    });
-  }
-});
-
-console.log(chalk.magenta(`Hi, I am hungry and want some sushi.`));
+cleanFiles("./basura");
