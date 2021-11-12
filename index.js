a6cbd98c72820974210d7f8ec54c463bdd9f5ce4
AipBorges <mpbds@uic.es>
2021-11-12 14:47:09 +0100

    delete is working now

--- index.js
+++ index.js
@@ -2,23 +2,43 @@ const chalk = require("chalk");
 const fs = require("fs");
 const path = require("path");
 
-const cleanFiles = (directory) => {
-  fs.readdir(directory, (error, files) => {
+const cleanAll = (directory) => {
+  fs.readdir(directory, (error, elements) => {
+    console.log(directory, elements);
     if (error) {
       console.log(chalk.cyan("Error reading your files."));
-      throw err;
     } else {
-      for (const file of files) {
-        fs.unlink(path.join(directory, file), (error) => {
+      for (const element of elements) {
+        console.log(element);
+        fs.stat(path.join(directory, element), (error, info) => {
           if (error) {
-            console.log(chalk.yellow("Error deleting your files."));
-            throw error;
+            console.log(chalk.redBright("Error stating elements."));
+          } else if (info.isFile()) {
+            console.log(chalk.cyan(`Here is file ${element}`));
+            fs.unlink(path.join(directory, element), (error) => {
+              error
+                ? console.log(chalk.yellow("Error deleting your files."))
+                : console.log(chalk.cyan(`We deleted your file ${element}.`));
+              process.exit(1);
+            });
+          } else if (info.isDirectory()) {
+            console.log(chalk.cyan(`Here is folder ${element}`));
+            fs.rmdir(path.join(directory, element), (error) => {
+              error
+                ? console.log(chalk.yellow("Error deleting your folder."))
+                : console.log(chalk.cyan(`We deleted your folder ${element}.`));
+            });
           }
-          console.log(chalk.cyan(`We deleted your file.`));
+
+          fs.rmdir(path.resolve(directory), (error) => {
+            error
+              ? console.log(chalk.yellow("Error deleting your folder."))
+              : console.log(chalk.cyan(`We deleted your folder ${element}.`));
+          });
         });
       }
     }
   });
 };
 
-cleanFiles("./basura");
+cleanAll("basura");
